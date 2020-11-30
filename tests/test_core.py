eacb9c073e7cbe6504ee83500f6ecfa05ec2c8b9
林玮 (Jade Lin) <linw1995@icloud.com>
2020-11-30 22:57:33 +0800

    refactor(core): use Hooker as a decorator
    do not use frame to hook the function
    
    BREAKING CHANGE: #1

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@ from hookers import hook
 
 
 def test_hook_func_call_before():
+    @hook
     def hello(name):
         return f"hello {name}"
 
@@ -9,22 +10,19 @@ def test_hook_func_call_before():
     kwargs = {}
     rv = "hello world"
 
-    with hook(hello) as hooker:
-        inputs = []
+    inputs = []
 
-        def dump_input(*args, **kwargs):
-            inputs.append((args, kwargs))
+    def dump_input(*args, **kwargs):
+        inputs.append((args, kwargs))
 
-        hooker.call_before(dump_input)
-
-        assert hello(*args, **kwargs) == rv
-        assert inputs == [(args, kwargs)]
+    hello.call_before(dump_input)
 
     assert hello(*args, **kwargs) == rv
     assert inputs == [(args, kwargs)]
 
 
 def test_temporary_hook_func_call_before():
+    @hook
     def hello(name):
         return f"hello {name}"
 
@@ -32,102 +30,25 @@ def test_temporary_hook_func_call_before():
     kwargs = {}
     rv = "hello world"
 
-    with hook(hello) as hooker:
-        inputs = []
+    inputs = []
 
-        def dump_input(*args, **kwargs):
-            inputs.append((args, kwargs))
+    def dump_input(*args, **kwargs):
+        inputs.append((args, kwargs))
 
-        with hooker.call_before(dump_input):
-            assert hello(*args, **kwargs) == rv
-            assert inputs == [(args, kwargs)]
-
+    with hello.call_before(dump_input):
         assert hello(*args, **kwargs) == rv
         assert inputs == [(args, kwargs)]
 
     assert hello(*args, **kwargs) == rv
     assert inputs == [(args, kwargs)]
 
-
-def test_hook_func_with_alias():
-    def hello(name):
-        return f"hello {name}"
-
-    args = ("world",)
-    kwargs = {}
-    rv = "hello world"
-
-    hello_alias = hello
-
-    with hook(hello) as hooker:
-        inputs = []
-
-        def dump_input(*args, **kwargs):
-            inputs.append((args, kwargs))
-
-        with hooker.call_before(dump_input):
-            assert hello_alias(*args, **kwargs) == rv
-            assert inputs == [(args, kwargs)]
-
-
-def test_hook_func_with_alias_outside():
-    def hello(name):
-        return f"hello {name}"
-
-    args = ("world",)
-    kwargs = {}
-    rv = "hello world"
-
-    hello_alias = hello
-
-    def inner_call():
-        with hook(hello) as hooker:
-            inputs = []
-
-            def dump_input(*args, **kwargs):
-                inputs.append((args, kwargs))
-
-            with hooker.call_before(dump_input):
-                assert hello_alias(*args, **kwargs) == rv
-                assert inputs == [(args, kwargs)]
-
-    inner_call()
-
-
-def test_hook_func_with_isolating_closure():
-    def hello(name):
-        return f"hello {name}"
-
-    args = ("world",)
-    kwargs = {}
-    rv = "hello world"
-
-    def make_closure(hello):
-        # frame 2
-        def inner_call():
-            # frame 3
-            # hello from frame 2
-            assert hello(*args, **kwargs) == rv
-            assert inputs == [(args, kwargs)]
-
-        return inner_call
-
-    # frame 1
-    # pass as argument before hooking
-    func = make_closure(hello)
-
-    with hook(hello) as hooker:
-        inputs = []
-
-        def dump_input(*args, **kwargs):
-            inputs.append((args, kwargs))
-
-        with hooker.call_before(dump_input):
-            func()
+    assert hello(*args, **kwargs) == rv
+    assert inputs == [(args, kwargs)]
 
 
 def test_hook_obj_method():
     class Person:
+        @hook
         def hello(self, name):
             return f"hello {name}"
 
@@ -136,12 +57,11 @@ def test_hook_obj_method():
     kwargs = {}
     rv = "hello world"
 
-    with hook(person.hello) as hooker:
-        inputs = []
+    inputs = []
 
-        def dump_input(*args, **kwargs):
-            inputs.append((args, kwargs))
+    def dump_input(self, *args, **kwargs):
+        inputs.append((self, args, kwargs))
 
-        with hooker.call_before(dump_input):
-            assert person.hello(*args, **kwargs) == rv
-            assert inputs == [(args, kwargs)]
+    with person.hello.call_before(dump_input):
+        assert person.hello(*args, **kwargs) == rv
+        assert inputs == [(person, args, kwargs)]
