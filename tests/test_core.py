f468676cb66505c272b0999f9079e4cf431aec32
林玮 (Jade Lin) <linw1995@icloud.com>
2020-11-30 23:02:26 +0800

    test(core): more unittest cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -65,3 +65,42 @@ def test_hook_obj_method():
     with person.hello.call_before(dump_input):
         assert person.hello(*args, **kwargs) == rv
         assert inputs == [(person, args, kwargs)]
+
+
+def test_hook_obj_method_isolatively():
+    class Person:
+        @hook
+        def hello(self, name):
+            return f"hello {name}"
+
+    jack = Person()
+    salra = Person()
+
+    inputs = []
+
+    def dump_input(self, *args, **kwargs):
+        inputs.append((self, args, kwargs))
+
+    with jack.hello.call_before(dump_input):
+        assert jack.hello("salra") == "hello salra"
+        assert inputs == [(jack, ("salra",), {})]
+
+        assert salra.hello("jack") == "hello jack"
+        assert inputs == [(jack, ("salra",), {})]
+
+
+def test_obj_hooked_method_got_overwrited():
+    class Person:
+        @hook
+        def hello(self, name):
+            return f"hello {name}"
+
+    person = Person()
+
+    def assert_not_called(self, *args, **kwargs):
+        assert 0
+
+    person.hello.call_before(assert_not_called)
+    person.hello = lambda x: f"hello {x}"
+    assert isinstance(Person.hello, hook)
+    assert person.hello("world") == "hello world"
